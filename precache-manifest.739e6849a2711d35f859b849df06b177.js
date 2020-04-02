733928ed27d75f56efa338672d910fa6623d3f91
magdyMohammed20 <magdymohammed37@yahoo.com>
2020-04-02 08:35:14 +0200

    Updates

---
+++ precache-manifest.739e6849a2711d35f859b849df06b177.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29db5bc91dfccc0994192109eef5d4b3",
+    "url": "/magdyMohammed20/todoApp/index.html"
+  },
+  {
+    "revision": "83e4f0f1f69ef5e8bb75",
+    "url": "/magdyMohammed20/todoApp/static/css/main.6fbd768a.chunk.css"
+  },
+  {
+    "revision": "25d01e4de46183aa4c71",
+    "url": "/magdyMohammed20/todoApp/static/js/2.c5504464.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/magdyMohammed20/todoApp/static/js/2.c5504464.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83e4f0f1f69ef5e8bb75",
+    "url": "/magdyMohammed20/todoApp/static/js/main.8b64bff1.chunk.js"
+  },
+  {
+    "revision": "64dd37a796b7bf9914e5",
+    "url": "/magdyMohammed20/todoApp/static/js/runtime-main.4c64c572.js"
+  }
+]);
